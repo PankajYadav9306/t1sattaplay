9a234c1e03f8dee4f4821cb06555963413a00f76
Your Name <you@example.com>
2025-09-29 21:53:41 +0530

    commit contact page

--- src/app/(root)/contact/Contact.jsx
+++ src/app/(root)/contact/Contact.jsx
@@ -1,31 +1,40 @@
+"use client";
+import { Typewriter } from "react-simple-typewriter";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Contact = () => {
+const Contact = ({ setting }) => {
   return (
-    <div className="container mx-auto px-3 mt-24">
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        <div className="bg-gradient2 px-3 items-center justify-center h-full min-h-56 flex flex-col gap-4">
-          <h2 className="text-white text-3xl md:text-4xl">Contact Us</h2>
-          <p className="text-base text-center text-white">
-            For any enquiry message us at{" "}
-            <Link
-              className="text-white font-semibold"
-              href="Contact@T1satta.In"
-            >
-              Contact@T1satta.In
-            </Link>
-          </p>
-        </div>
-        <div>
-          <Image
-            src="https://i.ibb.co/93nZ3LWy/Yellow-Minimalist-Contact-Us-Instagram-Post-2.png"
-            alt="contact img"
-            className="max-md:mt-[-50px] max-sm:mt-0 w-full"
-            width={300}
-            height={400}
+    <div className="mx-auto mt-24">
+      <div className="flex flex-col justify-center items-center bg-gray-100">
+        <h1 className="text-3xl mt-5 font-bold text-gray-800">
+          <Typewriter
+            words={["✨ ईमानदारी ही खाईवाल BHAI की पहचान है। 😎"]}
+            cursor
+            cursorBlinking={false}
+            cursorStyle=""
+            typeSpeed={80}
           />
+        </h1>
+        <div className="bg-gradient w-full text-center mt-5 py-4  ">
+          <h2 className="text-4xl lg:text-5xl text-white font-semibold text-theme-accent">
+            T1 Satta 👑
+          </h2>
+        </div>
+        <div className="mx-auto max-w-[300px] mt-4 hover:scale-110 transition-all duration-300">
+          <Link
+            target="_blank"
+            href={`https://wa.me/+${setting?.whatsappNumber}`}
+          >
+            <Image
+              className="max-sm:w-[200px] mx-auto max-sm:h-16"
+              width={300}
+              height={100}
+              src="https://i.ibb.co/4RJCLbSB/whatsapp.png"
+              alt="whatsapp"
+            />
+          </Link>
         </div>
       </div>
     </div>
